8e33a9346e0b92ce5f15cd434e7da2bd45854992
ZaslavskayaEG <zhuchok90@gmail.com>
2017-07-09 00:10:58 +1000

    Использование коллекций

--- address-book-web-tests/address-book-web-tests/appmanager/GroupHelper.cs
+++ address-book-web-tests/address-book-web-tests/appmanager/GroupHelper.cs
@@ -28,6 +28,20 @@ namespace WebAddressbookTests
             return this;
         }
 
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> groups = new List<GroupData>();
+
+            manager.Navigator.GoToGroupsPage();
+            ICollection<IWebElement> elements = driver.FindElements(By.CssSelector("span.group"));
+            foreach (IWebElement element in elements)
+            {
+                groups.Add(new GroupData(element.Text));
+            }
+
+            return groups;
+        }
+
         public GroupHelper Modify(int i, GroupData newData)
         {
             manager.Navigator.GoToGroupsPage();
--- address-book-web-tests/address-book-web-tests/tests/GroupCreationTests.cs
+++ address-book-web-tests/address-book-web-tests/tests/GroupCreationTests.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace WebAddressbookTests
@@ -16,7 +17,13 @@ namespace WebAddressbookTests
             group.Header = "ddd";
             group.Footer = "fff";
 
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
             app.Groups.Create(group);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
 
         }
 
@@ -27,7 +34,33 @@ namespace WebAddressbookTests
             group.Header = "";
             group.Footer = "";
 
+
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
             app.Groups.Create(group);
+
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
+        }
+
+        [Test]
+        public void BadNameGroupCreationTest()
+        {
+            GroupData group = new GroupData("a'a");
+            group.Header = "";
+            group.Footer = "";
+
+
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            app.Groups.Create(group);
+
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
         }
     }
 }
