7dc9030dd11781220539fd791f3fa448269256ca
ZaslavskayaEG <zhuchok90@gmail.com>
2017-07-08 23:42:45 +1000

    Примеры использования циклов

---
+++ address-book-web-tests/address-book-web-tests/tests/UnitTest2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace address_book_web_tests.tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            IWebDriver driver = null;
+            int attempt = 0;
+
+            do
+            {
+                System.Threading.Thread.Sleep(1000);
+                attempt++;
+            } while (driver.FindElements(By.Id("test")).Count > 0 && attempt < 60);
+            
+            // ....
+        }
+    }
+}
