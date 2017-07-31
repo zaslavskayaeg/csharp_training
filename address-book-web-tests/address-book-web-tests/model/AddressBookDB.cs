17479c8810cc38ab10621db61cf50082b68bdea0
ZaslavskayaEG <zhuchok90@gmail.com>
2017-07-31 17:59:35 +1000

    Установление соединения с базой данных

---
+++ address-book-web-tests/address-book-web-tests/model/AddressBookDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqToDB;
+
+namespace WebAddressbookTests
+{
+    class AddressBookDB : LinqToDB.Data.DataConnection
+    {
+        public AddressBookDB() : base("AddressBook") { }
+
+        public ITable<GroupData> Groups { get { return GetTable<GroupData>(); } }
+        public ITable<ContactData> Contucts { get { return GetTable<ContactData>(); } }
+    }
+}
